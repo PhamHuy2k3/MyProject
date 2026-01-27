6468d9f2c1117b58764a84964dae03562d5e4cd1
Pham Quoc Huy <itachihuy212@gmail.com>
2026-01-27 12:29:41 +0700

    new update

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MyApp',
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'MyProject.urls'
-
+TEMPLATES_dir = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_dir],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
